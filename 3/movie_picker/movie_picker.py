a52353769ab8c6b9b9c257cd0514ca6548d8d334
Liliia <beregechlili@gmail.com>
2024-05-23 09:12:45 +0300

    movie_picker (task 16)

--- 3/movie_picker/movie_picker.py
+++ 3/movie_picker/movie_picker.py
@@ -1,3 +1,5 @@
+import itertools
+
 if __name__ == '__main__':
     print('Tasks 15-17. Movie picker.')
 GENRES = {
@@ -8,20 +10,20 @@ GENRES = {
     'thriller': ['Vanilla Sky'],
     'action': ['Mission Impossible']
 }
-ACTORS = {
-    'Robert De Niro': ['Meet the Parents'],
-    'Ben Stiller': ['Meet the Parents'],
-    'Adam Sandler': ['Anger Management'],
-    'Jack Nicholson': ['Anger Management'],
-    'Brendan Fraser': ['Mummy'],
-    'Rachel Weisz': ['Mummy'],
-    'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
-    'Penelope Cruz': ['Vanilla Sky'],
-    'Cameron Diaz': ['Vanilla Sky'],
-    'Brad Pitt': ['Meet Joe Black'],
-    'Anthony Hopkins': ['Meet Joe Black'],
-    'Jeremy Renner': ['Mission Impossible']
-}
+#ACTORS = {
+#    'Robert De Niro': ['Meet the Parents'],
+#   'Ben Stiller': ['Meet the Parents'],
+#    'Adam Sandler': ['Anger Management'],
+#    'Jack Nicholson': ['Anger Management'],
+#    'Brendan Fraser': ['Mummy'],
+#   'Rachel Weisz': ['Mummy'],
+#   'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
+#   'Penelope Cruz': ['Vanilla Sky'],
+#   'Cameron Diaz': ['Vanilla Sky'],
+#   'Brad Pitt': ['Meet Joe Black'],
+#   'Anthony Hopkins': ['Meet Joe Black'],
+#   'Jeremy Renner': ['Mission Impossible']
+#}
 search_by_genre = input("Input: > Search by Genre: ")
 if search_by_genre == 'y':
     available_genres = GENRES.keys()
@@ -36,17 +38,47 @@ if search_by_genre == 'y':
     if user_movie in available_movies:
         print(f"Output: Movie to watch: {user_movie}. Genre: {user_genre}.")
 
+# if search_by_genre == 'n':
+#     search_by_actor = input("Input: > Search by Actor: ")
+#     if search_by_actor == 'y':
+#         available_actors = ACTORS.keys()
+#         print(f"Output: Available Actors: {available_actors}")
+#
+#         actor = input("Input: > Enter actor: ")
+#         if actor in available_actors:
+#             movies = ACTORS[actor]
+#             print(f"Output: Available Movies: {movies}")
+#
+#             user_movie = input("Input: > Enter movie: ")
+#             if user_movie in movies:
+#                 print(f"Output: Movie to watch: {user_movie}. Actor: {actor}.")
+
+CAST = {
+    'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+    'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+    'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+    'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+    'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+    'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
+}
+
 if search_by_genre == 'n':
-    search_by_actor = input("Input: > Search by Actor: ")
-    if search_by_actor == 'y':
-        available_actors = ACTORS.keys()
-        print(f"Output: Available Actors: {available_actors}")
+    search_by_cast = input("Input: > Search by Actor: ")
+    if search_by_cast == 'y':
+        all_actors = []
+        for actors_list in CAST.values():
+            all_actors=all_actors+actors_list
+
+        print(f"Output: Available Actors: {all_actors}")
 
         actor = input("Input: > Enter actor: ")
-        if actor in available_actors:
-            movies = ACTORS[actor]
-            print(f"Output: Available Movies: {movies}")
+        available_movies=[]
+        for movie, actors_list in CAST.items():
+            if actor in actors_list:
+                available_movies.append(movie)
 
-            user_movie = input("Input: > Enter movie: ")
-            if user_movie in movies:
-                print(f"Output: Movie to watch: {user_movie}. Actor: {actor}.")
+        print(f"Available movies: {available_movies} with {actor}")
+        user_movie = input("Input: > Enter movie: ")
+        print(f"Available movies: {available_movies} with {actor}")
+        if user_movie in available_movies:
+            print(f"Output: Movie to watch: {user_movie}. Starring: {actor}.")
