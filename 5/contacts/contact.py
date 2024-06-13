a7061fa874d53a378397539ca750e27534fb5daf
Liliia <beregechlili@gmail.com>
2024-06-13 14:27:14 +0300

    unique (task 15)

--- 5/contacts/contact.py
+++ 5/contacts/contact.py
@@ -1,30 +1,33 @@
-contact = {
-    'name': None,
-    'email': None,
-    'age': None
-}
-
-
-def validate_name(name):
+def validate_name(name: str):
     if len(name) > 50:
         raise ValueError('Name is too large!')
 
+    return name
 
-def validate_email(email):
+
+def validate_email(email: str):
     if '@' not in email or '.' not in email:
         raise ValueError('Invalid email!')
 
+    return email
 
-def validate_age(age):
+
+def validate_age(age: int):
     try:
-        age = int(age)  # Python will raise ValueError if not numeric
         if age <= 0:
             # We ask Python to raise ValueError if <= 0
             raise ValueError
     except ValueError:
         raise ValueError('Invalid age!')
 
+    return age
+
 
 class Contact:
-    # TODO: implement instead of above
-    ...
+    def __init__(self, name: str, email: str, age: int):
+        self.name = validate_name(name)
+        self.email = validate_email(email)
+        self.age = validate_age(age)
+
+    def __str__(self):
+        return f"{{'name': {self.name},'email': {self.email},'age': {self.age}}}"
